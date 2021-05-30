ab6bf6865778de3cf7866938e46a102428ea90e5
avnish ranwa <avnishranwa@jklu.edu.in>
2021-05-30 20:09:15 +0530

    complete

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAt7TV02xo6vesQmp5fNMsjm9Ev_9IU-Z4",
+    authDomain: "scribd-7ecbb.firebaseapp.com",
+    projectId: "scribd-7ecbb",
+    storageBucket: "scribd-7ecbb.appspot.com",
+    messagingSenderId: "159285095525",
+    appId: "1:159285095525:web:9b4712c25bbdf074f26a8c"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
